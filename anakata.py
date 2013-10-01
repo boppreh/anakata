3e6c75c080589f7a9200be0ba80f8ee3a8a472b2
BoppreH <boppreh@gmail.com>
2013-10-01 02:27:43 -0300

    Add composite object support

--- anakata.py
+++ anakata.py
@@ -4,23 +4,27 @@ world_side = 7
 
 class MovementException(Exception): pass
 
-class Cell(object):
-    def __init__(self, pos, char):
-        self.pos = pos
+class Object(object):
+    def __init__(self, cells, char):
+        self.cells = cells
         self.char = char
 
-    def move(self, delta_pos):
-        new_pos = tuple(i + j for i, j in zip(self.pos, delta_pos))
-        for i in new_pos:
-            if i < 0 or i >= world_side:
-                raise MovementException()
+    def move(self, movement):
+        new_cells = []
+        for cell in self.cells:
+            new_cell = tuple(i + j for i, j in zip(cell, movement))
+            for i in new_cell:
+                if i < 0 or i >= world_side:
+                    raise MovementException()
+            new_cells.append(new_cell)
 
-        self.pos = new_pos
+        self.cells = new_cells
 
     def __str__(self):
-        return self.char
+        return self.char + ' ' + ' '.join(map(str, self.cells))
 
-player = Cell((0, 0, 0, 0), '@')
+
+player = Object([(0, 0, 0, 0)], '@')
 
 directions_by_key = {
     'w': (0, 0, 1, 0),
@@ -39,4 +43,4 @@ while True:
 
     direction = directions_by_key[command]
     player.move(direction)
-    print player.pos
+    print player
