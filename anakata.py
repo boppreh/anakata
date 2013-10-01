918e1f148a466962b85c039fd9c46bf024a37b65
BoppreH <boppreh@gmail.com>
2013-10-01 02:36:08 -0300

    Add map printing

--- anakata.py
+++ anakata.py
@@ -24,7 +24,8 @@ class Object(object):
         return self.char + ' ' + ' '.join(map(str, self.cells))
 
 
-player = Object([(0, 0, 0, 0)], '@')
+player = Object([(3, 3, 3, 3)], '@')
+objects = [player]
 
 directions_by_key = {
     'w': (0, 0, 1, 0),
@@ -36,11 +37,29 @@ directions_by_key = {
     'M': (1, 0, 0, 0),
     'K': (-1, 0, 0, 0),
 }
+import sys
 while True:
+    sys.stdout.write('\n' * 10)
+    for z in reversed(range(world_side)):
+        for y in reversed(range(world_side)):
+            for w in reversed(range(world_side)):
+                for x in range(world_side):
+                    empty = True
+                    for o in objects:
+                        if (x, y, z, w) in o.cells:
+                            empty = False
+                            sys.stdout.write(o.char)
+                    if empty:
+                        sys.stdout.write('.')
+
+                sys.stdout.write(' ')
+            sys.stdout.write('\n')
+        sys.stdout.write('\n\n')
+
     command = msvcrt.getch()
     if command not in directions_by_key:
         continue
 
     direction = directions_by_key[command]
     player.move(direction)
-    print player
+
